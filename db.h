3e151afd32c543eb0c83ca91c4d33125215c137c
Justin-Lamberty <111925736+Justin-Lamberty@users.noreply.github.com>
2025-03-18 14:18:13 +0000

    initial commit

--- db.h
+++ db.h
@@ -1,1 +1,50 @@
+#ifndef DB_H
+#define DB_H
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+class Database {
+private:
+    std::string db;
+    std::string username;
+    std::string password;
+    bool connected = false;
+
+    static Database* instance;
+
+    // Private constructor
+    Database(const std::string& dbName, const std::string& user, const std::string& pass);
+
+    // Private destructor
+    ~Database();
+
+public:
+    // Delete copy constructor and assignment to prevent copying
+    Database(const Database&) = delete;
+    Database& operator=(const Database&) = delete;
+
+    static Database* getInstance(const std::string& dbName, const std::string& user, const std::string& pass);
+
+    void connect();
+    void disconnect();
+    bool isConnected() const;
+
+    // Overload new and delete
+    void* operator new(size_t size);
+    void operator delete(void* ptr);
+
+    // Setters and Getters
+    void set_username(const std::string& user);
+    std::string get_username() const;
+
+    void set_password(const std::string& pass);
+    std::string get_password() const;
+
+    // Reset the instance
+    static void resetInstance();
+};
+
+#endif // DB_H
 
--- dp.cpp
+++ dp.cpp
@@ -1,1 +1,72 @@
+#include "db.h"
+#include <cstdlib> // for malloc and free
+
+Database* Database::instance = nullptr;
+
+Database::Database(const std::string& dbName, const std::string& user, const std::string& pass)
+    : db(dbName), username(user), password(pass), connected(false) {}
+
+Database::~Database() {
+    if (connected) {
+        disconnect();
+    }
+}
+
+Database* Database::getInstance(const std::string& dbName, const std::string& user, const std::string& pass) {
+    if (instance == nullptr) {
+        instance = new Database(dbName, user, pass);
+    } else {
+        if (instance->db != dbName || instance->username != user || instance->password != pass) {
+            throw std::runtime_error("invalid database name, username or password");
+        }
+    }
+    return instance;
+}
+
+void Database::connect() {
+    connected = true;
+}
+
+void Database::disconnect() {
+    connected = false;
+}
+
+bool Database::isConnected() const {
+    return connected;
+}
+
+void* Database::operator new(size_t size) {
+    std::cout << "overloaded new ";
+    void* ptr = std::malloc(size);
+    if (!ptr) throw std::bad_alloc();
+    return ptr;
+}
+
+void Database::operator delete(void* ptr) {
+    std::cout << "overloaded delete ";
+    std::free(ptr);
+}
+
+void Database::set_username(const std::string& user) {
+    username = user;
+}
+
+std::string Database::get_username() const {
+    return username;
+}
+
+void Database::set_password(const std::string& pass) {
+    password = pass;
+}
+
+std::string Database::get_password() const {
+    return password;
+}
+
+void Database::resetInstance() {
+    if (instance != nullptr) {
+        delete instance;
+        instance = nullptr;
+    }
+}
 
